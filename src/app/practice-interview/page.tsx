e7fbf8829ec5c09e3c139b4ecc67dede6bc06b39
Hamideh Nouri <nouri.hamideh@gmail.com>
2026-07-11 17:17:21 +0200

    Remove unused page

--- src/app/practice-interview/page.tsx
+++
@@ -1,6 +0,0 @@
-import PracticeInterview from "@/components/PracticeInterview";
-
-export default async function Page({ searchParams }: { searchParams: Promise<{ q?: string; category?: string }> }) {
-  const sp = await searchParams;
-  return <PracticeInterview question={sp.q ?? ""} category={sp.category ?? ""} />;
-}
